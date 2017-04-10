cb33c0ff2b3b190364a424b8950e6528c2f48a5a
liangzelei <luckyforlei@163.com>
2017-04-10 16:17:08 +0800

    增加路由模块

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,51 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 import ReactDOM from 'react-dom';
-import cssStyle  from '../css/body.css';
+import cssStyle from '../css/body.css';
+
+import {
+    Input
+} from 'antd';
+import 'antd/dist/antd.css';
+
 export default class Index extends Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        isClick : true
-      };
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClick: true
+        };
     };
-    onClickWithDiv(){
-      this.setState({
-        isClick : !this.state.isClick
-      })
+    onClickWithDiv() {
+        this.setState({
+            isClick: !this.state.isClick
+        })
     };
     render() {
         var styles = {
             contexts: {
                 backgroundColor: "#ffba02",
-                color          : 　 "#ffffff",
-                fontSize       : (this.state.isClick) ? "42px" : "12px",
-                marginTop      : (this.state.isClick) ? "20px" : "5px"
+                color: 　 "#ffffff",
+                fontSize: (this.state.isClick) ? "42px" : "12px",
+                marginTop: (this.state.isClick) ? "20px" : "5px"
             }
         };
-        return (
-          <div>
-            <div style = {styles.contexts} onClick={this.onClickWithDiv.bind(this)}> Welcome to React! </div>
-            <div className={this.state.isClick ? cssStyle.maxHighLight : cssStyle.mixHighLight} onClick={this.onClickWithDiv.bind(this)}> Welcome to React! </div>
-          </div>
+        return ( <
+            div >
+            <
+            div style = {
+                styles.contexts
+            }
+            onClick = {
+                this.onClickWithDiv.bind(this)
+            } > Welcome to React! < /div> <
+            div className = {
+                this.state.isClick ? cssStyle.maxHighLight : cssStyle.mixHighLight
+            }
+            onClick = {
+                this.onClickWithDiv.bind(this)
+            } > Welcome to React! < /div> <
+            /div>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,10 @@ const config = {
      //下面是添加css
      {
        test: /\.css?$/,
-       loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]'
+       //加载ant design时使用
+       loader: 'style-loader!css-loader'
+       //加载CSS模块化时使用
+       //loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]'
      },
    ]
   },
